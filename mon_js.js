185aa4ae6a83d79894b958297af44ad591b3ede5
charlenemelin <116005778+charlenemelin@users.noreply.github.com>
2022-10-21 02:30:52 +0200

    Add files via upload

---
+++ mon_js.js
@@ -0,0 +1,8 @@
+function afficheImage() {
+    let monImage = document.getElementById("monImageAxolotl");
+    monImage.classList.remove("cacher");
+    let maDiv = document.getElementById("maDivAxolotl");
+    maDiv.classList.remove("fadeout");
+    maDiv.classList.add("fadein");
+}
+
